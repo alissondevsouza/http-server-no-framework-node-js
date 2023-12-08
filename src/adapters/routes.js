e2182e0e544b323f4e47c9a658a7ff4c22ff53ed
alissondevsouza <alisson.rhuan@crearesistemas.com.br>
2023-12-08 15:39:58 -0300

    Feat: Created product routes

--- src/adapters/routes.js
+++ src/adapters/routes.js
@@ -4,9 +4,27 @@ export default class Routes {
 
     execute(keyRouter) {
         
-        return {
-            code: 200,
-            message: 'Testando api'
+        let productId = null;
+        if (keyRouter.includes('id=')) productId = keyRouter.match(/\d+/)[0];
+
+        switch (true) {
+            case keyRouter === '/product:get':
+                return { code: 200, message: 'Get Product' };
+
+            case keyRouter === `/product?id=${productId}:get`:
+                return { code: 200, message: 'Get Product Id' };
+
+            case keyRouter === '/product:post':
+                return { code: 200, message: 'Post Product' };
+
+            case keyRouter === `/product?id=${productId}:put`:
+                return { code: 200, message: 'Put product Id' };
+            
+            case keyRouter === `/product?id=${productId}:delete`:
+                return { code: 200, message: 'Delete product Id' };
+
+            default:
+                return { code: 404, message: 'Not Found' };
         }
 
     }
